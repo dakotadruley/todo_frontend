d98a813d618ce061fe5522e1a454bc591fbf27a1
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-02-26 19:06:06 -0800

    added

--- todo_frontend/src/TodoApp.js
+++ todo_frontend/src/TodoApp.js
@@ -6,6 +6,48 @@ export default class TodoApp extends Component {
     state = { todos: [] }
 
     componentDidMount = async () => {
+        const user = JSON.parse(localStorage.getItem('user'));
+        const todos = await request.get(`https://agile-bastion-07503.herokuapp.com:${process.env.REACT_APP_BACK_END_PORT}/api/todos`)
+            .set('Authorization', user.token);
         
+        this.setState({ todos: todos.body })
+    }
+
+    handleClick = async () => {
+        const newTodo = {
+            id: Math.random(),
+            task: this.state.todoInput,
+            complete: false,
+        };
+
+        const user = JSON.parse(localStorage.getItem('user'));
+
+        const newTodos = [...this.state.todos, newTodo];
+
+        this.setState({ todos: newTodos });
+        const data = await request.post(`https://agile-bastion-07503.herokuapp.com:${process.env.REACT_APP_BACK_END_PORT}/api/todos`, {
+            task: this.state.todoInput
+        })
+            .set('Authorization', user.token);
+    }
+
+    handleInput = (e) => { this.setState({ todoInput: e.target.value })};
+
+    render() {
+        if (localStorage.getItem('user')) {
+            return (
+                <>
+                <h2>{JSON.parse(localStorage.getItem('user')).email}</h2>
+                <AddTodo
+                todoInput={ this.state.todoInput }
+                handleClick={ this.handleClick }
+                handleInput={ this.handleInput } />
+                </>
+
+                {
+                    this.state.todos.map((todo) => )
+                }
+            )
+        }
     }
 }
