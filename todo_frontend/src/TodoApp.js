30b5d1e0625e97c529c2fbcc92ace7003a3ac679
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-02-26 17:12:33 -0800

    this

---
+++ todo_frontend/src/TodoApp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import AddTodo from './AddTodo.js';
+import request from 'superagent';
+
+export default class TodoApp extends Component {
+    state = { todos: [] }
+
+    componentDidMount = async () => {
+        
+    }
+}
